808d5d5c1a3351e53a8c08b38ddd799779068f39
victoralonsorodriguez <victoralonsorodriguez61@gmail.com>
2024-07-28 23:13:16 +0200

    Simplify move plots

--- py_scripts/py_move_plots.py
+++ py_scripts/py_move_plots.py
@@ -1,16 +1,16 @@
-import os
-import shutil
-
 
 # This script moves the plots created to a new folder
 # inside the main galaxy directory
+
+import os
+import shutil
 
 
 cwd = os.getcwd()
 galaxy = (cwd.split('/')[-1])
 
-# For plots with the data
-plot_folder = f'{galaxy}_plots_galfit'
+# New folder to store the plots
+plot_folder = f'{galaxy}_plots'
 
 if os.path.isdir(plot_folder) == True:
     
@@ -18,31 +18,13 @@ if os.path.isdir(plot_folder) == True:
 
 os.mkdir(plot_folder)
 
+# Moving the plots
 for file in os.listdir(cwd):
 
-    if '.pdf' in file and 'plot' in file and 'galfit' in file:
+    if '.pdf' in file and 'plot' in file and ('galfit' in file or 'ratios' in file):
             
             file_original_path = os.path.join(cwd, file)
             file_new_path = os.path.join(cwd,f'{plot_folder}',file)
             
             shutil.copyfile(file_original_path,file_new_path)
             os.remove(file_original_path)            
-
-# For plots with ratios
-plot_folder = f'{galaxy}_plots_ratios'
-
-if os.path.isdir(plot_folder) == True:
-    
-    shutil.rmtree(f'{cwd}/{plot_folder}')
-
-os.mkdir(plot_folder)
-
-for file in os.listdir(cwd):
-
-    if '.pdf' in file and 'plot' in file and 'ratios' in file:
-            
-            file_original_path = os.path.join(cwd, file)
-            file_new_path = os.path.join(cwd,f'{plot_folder}',file)
-            
-            shutil.copyfile(file_original_path,file_new_path)
-            os.remove(file_original_path)
