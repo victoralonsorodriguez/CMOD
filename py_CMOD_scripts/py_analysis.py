6355cb1be73489d7af1db290fdbe2652726c115e
Victor Alonso Rodriguez <victoralonsorodriguez61@gmail.com>
2025-10-19 10:23:30 +0100

    py_galfit/imfit_scripts - constrains: moved to fitting.py - other modules dependencies fixed

--- py_CMOD_scripts/py_analysis.py
+++ py_CMOD_scripts/py_analysis.py
@@ -26,23 +26,20 @@ import time
 import warnings
 warnings.filterwarnings('ignore')
 
-from py_galfit_constraints_script import create_constraints
-from py_galfit_constraints_values import constraints_values
 from py_galfit_initial_params import initial_params
 from py_galfit_initial_params_script import create_initiaL_params
-from py_galfit_script import create_script
-from py_imfit_script import create_conf_imfit
-
 
 from py_initial_condition import initial_conditions
 from py_config_argparse import argparse_values
 from py_version_control import version_directory, version_file, version_file_last
+from py_psf import create_psf
+
+from cmod.fitting import constraints_values, create_constraints, create_script, create_conf_imfit
 from cmod.io import open_fits
 from cmod.photometry import fits_mag_to_counts, values_counts_to_mag
 from cmod.processing import max_center_value, isophote_fitting
 from cmod.results import galfit_init_dataframe, galfit_create_dataframe,imfit_init_dataframe,imfit_create_dataframe
 from cmod.utils import Cronometro, round_number, create_folder, rad_to_deg_abs
-from py_psf import create_psf
 
 
 def analisys():
